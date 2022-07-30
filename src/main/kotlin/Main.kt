a4d863a37b796725b5b68f572b1bbd295d8978d0
Rerumu <rerumu@protonmail.com>
2022-07-30 19:10:07 -0400

    Make program generate `.jar` files

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,24 @@
 import bytecode.Module
 import bytecode.ModuleReader
 import codegen.ObjectBuilder
+import net.bytebuddy.ByteBuddy
+import template.Auxiliary
+import types.*
 import java.io.File
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
+import java.util.jar.Attributes
+import java.util.jar.Manifest
+
+fun getManifest(): Manifest {
+    val manifest = Manifest()
+    val attr = manifest.mainAttributes
+
+    attr[Attributes.Name.MANIFEST_VERSION] = "1.0"
+    attr[Attributes.Name.MAIN_CLASS] = "template.Program"
+
+    return manifest
+}
 
 fun getModuleOf(name: String): Module {
     val data = File(name).readBytes()
@@ -14,6 +29,27 @@ fun getModuleOf(name: String): Module {
     return ModuleReader.readModule(buffer)
 }
 
+fun addLoadedClass(file: File, klass: Class<out Any>) {
+    ByteBuddy().redefine(klass).make().inject(file)
+}
+
+fun addBuiltIn(file: File) {
+    addLoadedClass(file, ValueType::class.java)
+    addLoadedClass(file, NilType::class.java)
+    addLoadedClass(file, BooleanType::class.java)
+    addLoadedClass(file, NumberType::class.java)
+    addLoadedClass(file, StringType::class.java)
+    addLoadedClass(file, TableType::class.java)
+    addLoadedClass(file, ClosureType::class.java)
+
+    addLoadedClass(file, Auxiliary::class.java)
+    addLoadedClass(file, BuiltIn::class.java)
+
+    for (tuple in BuiltIn.INSTANCE.map) {
+        addLoadedClass(file, tuple.value.javaClass)
+    }
+}
+
 fun main(file_list: Array<String>) {
     if (file_list.size != 1) {
         println("Must provide exactly one file!")
@@ -21,11 +57,12 @@ fun main(file_list: Array<String>) {
     }
 
     val builder = ObjectBuilder(getModuleOf(file_list[0]))
-    val saving = File("working_at")
+    val file = File("working_at/Luau.jar")
 
-    builder.genStringList().saveIn(saving)
+    builder.genEntryPoint().toJar(file, getManifest())
 
-    for (ready in builder.genClosureList()) {
-        ready.saveIn(saving)
-    }
+    builder.genStringList().inject(file)
+    builder.genClosureList().forEach { it.inject(file) }
+
+    addBuiltIn(file)
 }
